aabd5836569403453aa910237014a916528b864e
Eric Christopherson <echristopherson@gmail.com>
2018-02-08 20:45:38 -0600

    fix typo in exercise 5's comment

--- src/chapter01.clj
+++ src/chapter01.clj
@@ -62,7 +62,7 @@
    (exercise04)
    (apply mapset args)))
 
-;;; 5. Create a function that’s similar to symmetrize-body-parts except that it has to work with weird space aliens with radial symmetry. Instead of two eyes, arms, legs, and so on, they have  ve.
+;;; 5. Create a function that’s similar to symmetrize-body-parts except that it has to work with weird space aliens with radial symmetry. Instead of two eyes, arms, legs, and so on, they have five.
 
 ;;; 6. Create a function that generalizes symmetrize-body-parts and the func- tion you created in Exercise 5. The new function should take a col- lection of body parts and the number of matching body parts to add.
 ;;;    If you’re completely new to Lisp languages and functional program- ming, it probably won’t be obvious how to do this. If you get stuck, just move on to the next chapter and revisit the problem later.
